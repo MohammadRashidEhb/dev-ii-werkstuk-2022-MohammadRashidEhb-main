8e5881c4f5b1f448379f39eae0b2c550795b04dd
MohammadRashidEhb <mohammad.rashid@student.ehb.be>
2022-05-15 19:00:51 +0200

    added class, fixed sova bug & background position fixed

---
+++ js/class.js
@@ -0,0 +1,27 @@
+"use strict";
+
+export class Agent {
+    //...
+    constructor(name, role, image, description,abilities) {
+        this.name = this.getName(name);
+        this.role = this.getRole(role);
+        this.image = image;
+        this.description = this.getDescription(description);
+        this.abilities = abilities;
+        this.DisplayName = name;
+    }
+
+    getName(name) {
+        return 'name: ' + name;
+    }
+
+    getRole(role) {
+        return 'role: ' + role;
+     }
+
+    getDescription(description) {
+        return 'description: ' + description;
+    }
+    
+}
+
--- js/index.js
+++ js/index.js
@@ -1,20 +1,35 @@
 "use strict";
 
-const app = {
-  init: function () {
+import { Agent } from "./class.js";
 
+const agentsList= [];
+
+let counter = -1;
+const app = {
+  
+  init: function () {
     fetch('https://valorant-api.com/v1/agents')
     .then(response => {
     return response.json();
     }).then(data => {
 
+      for(let agent of data.data)
+      {
+        if(agent.role !== null)
+        {
+          counter++;
+          agentsList[counter] = new Agent(agent.displayName, agent.role.displayName, 
+            agent.displayIcon, agent.description, agent.abilities);
+          
+        }
+        
+      }
      
     //Displays the agents
-     displayAgents(data.data);
+     displayAgents(agentsList);
      //add chart to the page
      chartTable();
     }).catch(err => {
-    //log the error
     console.log(err);
    });
 
@@ -28,47 +43,44 @@ function displayAgents(agents) {
   document.getElementById('agentsList').innerHTML = "";
 
   agentsRoles = [0, 0, 0, 0];
+  
 
   for(let agent of agents)
       {
-        if(agent.role !== null)
-        {
+        
          // console.log(agent.role.displayName);
-          if(agent.role.displayName === "Duelist")
+          if(agent.role.includes("Duelist"))
           {
             agentsRoles[0]++
-          }else if(agent.role.displayName === "Initiator")
+          }else if(agent.role.includes("Initiator"))
           {
             agentsRoles[1]++
-          }else if(agent.role.displayName === "Sentinel")
+          }else if(agent.role.includes("Sentinel"))
           {
             agentsRoles[2]++
-          } else if(agent.role.displayName === "Controller")
+          } else if(agent.role.includes("Controller"))
           {
             agentsRoles[3]++
           }
          
-        }
        
         //console.log(agent.displayName); 
-        if (agent.role == null) 
-        { 
-          console.log(undefined);
-        }
-        else if (role == 'all') {
+        
+         if (role == 'all') {
+         // console.log(agent);
           document.getElementById('agentsList').innerHTML += `
-        <div class="agent" id=${agent.displayName}>
-          <img src="${agent.displayIcon}" alt="${agent.displayName}">
-          <h1>${agent.displayName}</1h>
+         <div class="agent" id=${agent.DisplayName}>
+          <img src="${agent.image}" alt="${agent.name}">
+          <h1>${agent.DisplayName}</1h>
         </div>
         `;
         }
         
-        else if (agent.role.displayName.toLowerCase() == role.toLowerCase()) {
+        else if (agent.role.toLowerCase().includes(role.toLowerCase()) ) {
           document.getElementById('agentsList').innerHTML += `
-        <div class="agent" id=${agent.displayName}>
-          <img src="${agent.displayIcon}" alt="${agent.displayName}">
-          <h1>${agent.displayName}</1h>
+          <div class="agent" id=${agent.DisplayName}>
+          <img src="${agent.image}" alt="${agent.name}">
+          <h1>${agent.DisplayName}</1h>
         </div>
         `;
         }
@@ -79,14 +91,15 @@ function displayAgents(agents) {
     const agentsBtn = document.getElementsByClassName('agent');
     for (let agentBtn of agentsBtn) {
       agentBtn.addEventListener('click', function () {
-        console.log(this.id);
+        //console.log(this.id);
         
-        // display agent data
-        for(let agent of agents)
+        //display agent data
+        for(let agent of agentsList)
         {
-          if(agent.displayName == this.id)
+             
+          if(agent.DisplayName == this.id)
           {
-            //console.log(agent.displayName);
+            console.log(this.id);    
             getAgent(agent);
           }
         }
@@ -107,17 +120,21 @@ function displayAgents(agents) {
     }
 }
 
+
+
 // pick agent data to display description
 function getAgent(agent) {
+  
+  console.log(agent);
   
   let agentDescriptionDiv =document.getElementById('agentDescription');
   agentDescriptionDiv.innerHTML = "";
   agentDescriptionDiv.innerHTML += `
-  <div class="agentDescription" style= background-color: rgb(30, 30, 30)>
-    <img src="${agent.displayIcon}" alt="${agent.displayName}">
-    <h1>Name: ${agent.displayName}</1h>
-    <h2>Role: ${agent.role.displayName}</2h>
-    <h3>Description: ${agent.description}</3h>
+  <div class="agentDescription" >
+    <img src="${agent.image}" alt="${agent.displayName}">
+    <h1>${agent.name}</1h>
+    <h2>${agent.role}</2h>
+    <h3>${agent.description}</3h>
     <h2>Abilities:</2h>
     <ul>
       <li>
@@ -144,7 +161,6 @@ function getAgent(agent) {
         </h1>
         <p> ${agent.abilities[3].description}</p>
       </li>
-      
     </ul>
    
   </div>`;
@@ -154,7 +170,7 @@ function getAgent(agent) {
 
 //make a chart table of the agents displayName
 function chartTable() {
-
+  
   const data = {
   labels: [
     "Duelist",
@@ -163,14 +179,21 @@ function chartTable() {
     "Controller",
   ],
   datasets: [{
-    label: 'My First Dataset',
+    label: "" ,
     data: [agentsRoles[0], agentsRoles[1], agentsRoles[2], agentsRoles[3]],
     backgroundColor: [
       'rgba(255, 99, 132)',
       'rgba(255, 159, 64)',
       'rgba(255, 205, 86)',
       'rgba(75, 192, 192)'
-    ]
+    ],
+    borderColor: [
+      'rgba(255, 99, 132)',
+      'rgba(255, 159, 64)',
+      'rgba(255, 205, 86)',
+      'rgba(75, 192, 192)'
+    ],
+    borderWidth: 1
   }]
  };
 
